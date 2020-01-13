6cfe764f53a38de1a67aba51d6949709ca0a946b
Caleb Lagge <caleb.i.lagge-1@ou.edu>
2020-01-12 23:00:30 -0600

    The initial practice commit

---
+++ R/myf.R
@@ -0,0 +1,14 @@
+#' Squared function
+#'
+#' Takes a vector and returns the vetor with its components squared
+#'
+#' @param x
+#'
+#' @return a vetor of squared components
+#' @export
+#'
+#' @examples
+#' x=1:30; myf(x)
+myf=function(x){
+  x^2
+}
